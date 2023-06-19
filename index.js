2ee8cd7ffd3fe5c5b69ed497fcb1f98f35354533
TCSAWSBU <137064157+TCSAWSBU@users.noreply.github.com>
2023-06-19 15:32:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 4000;
 
 app.get('/health', (req, res) => {
-  res.status(200).json('Healthy')
+  res.status(200).json('blue')
 });
 
 // // Create a connection pool to the PostgreSQL database
